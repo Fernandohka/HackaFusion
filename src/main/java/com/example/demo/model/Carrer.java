8e27172d013c8d8ae6f2948dc06817a365c28d38
CrCardd <cris.rc377@gmail.com>
2024-12-03 11:50:17 -0300

    Make Topic

---
+++ src/main/java/com/example/demo/model/Carrer.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Carrer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    
+}
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -44,85 +44,9 @@ public class User {
     private String description;
 
     @OneToMany(mappedBy = "user")
-    private Set<Permission> permissions;         
+    private Set<Ability> abilitys;
+    
+    @OneToMany(mappedBy = "user")
+    private Set<Carrer> carrers;
 
-    public Set<Permission> getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(Set<Permission> permissions) {
-        this.permissions = permissions;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEdv() {
-        return edv;
-    }
-
-    public void setEdv(String edv) {
-        this.edv = edv;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public boolean isAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-
-    public String getEts() {
-        return ets;
-    }
-
-    public void setEts(String ets) {
-        this.ets = ets;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
