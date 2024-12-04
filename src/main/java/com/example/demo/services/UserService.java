ff5723581a49d9d4cd95a0dc60949c40d55ed447
CrCardd <cris.rc377@gmail.com>
2024-12-04 13:35:23 -0300

    Dev with Models and Services

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import com.example.demo.dto.ResponseDto;
 
 public interface UserService {
-    public User post(String nome, String edv, String email, String password, String numero, Boolean admin, Boolean ets, String image, String description);
+    public User create(String nome, String edv, String email, String password, String numero, Boolean admin, Boolean ets, String image, String description);
     public List<User> getAll(Integer page, Integer size);
     public User getById(Long idUser);
     public ResponseDto delete(Long idUser);
-    public ResponseDto update(User user);
+    public ResponseDto update(Long idUser,String nome, String edv, String email, String password, String numero, Boolean admin, Boolean ets, String image, String description);
 }
