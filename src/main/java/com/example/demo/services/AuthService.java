949303d871058044271c795d6220f9b16954cf41
Fernandohka <Fernando2005andrade@gmail.com>
2024-12-04 11:43:27 -0300

    auth Service

--- src/main/java/com/example/demo/services/AuthService.java
+++ src/main/java/com/example/demo/services/AuthService.java
@@ -1,5 +1,10 @@
 package com.example.demo.services;
 
 public interface AuthService {
-    
+    public User login(String login, String password);
+    public Boolean validateEdvIsFree(String edv);
+    public Boolean validateEdv(String edv);
+    public Boolean validateEmailIsFree(String email);
+    public Boolean validateEmail(String email);
+    public Boolean ValidatePassword(String password);
 }
